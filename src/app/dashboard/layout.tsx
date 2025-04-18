0eb5305080866d5a7a6c928bc61d208c69466848
Mohit Arora <mannyisha808@gmail.com>
2025-04-18 15:58:00 +0530

    fixed

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
 import { ReactNode } from "react";
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
-// import { isAuthenticated } from "@/lib/actions/auth.action";
+import { isAuthenticated } from "@/lib/actions/auth.action";
 
 import "./dashboard.css"
 
 const Layout = async ({ children }: { children: ReactNode }) => {
-  // const isUserAuthenticated = await isAuthenticated();
-  // if (!isUserAuthenticated) redirect("/sign-in");
+  const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) redirect("/signup");
 
   return (
     <div className="root-layout">
--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,4 +1,4 @@
-import { initializeApp, getApps, applicationDefault } from "firebase-admin/app";
+import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
@@ -8,8 +8,11 @@ function initFirebaseAdmin() {
 
   if (!apps.length) {
     initializeApp({
-      credential: applicationDefault(),
-      databaseURL: "https://crypto-static-data-default-rtdb.firebaseio.com/"
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      }),
     });
   }
 
